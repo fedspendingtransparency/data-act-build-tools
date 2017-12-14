158076c5feb5817dc82f3f7b7b7fd5081bebcf66
Michael Hess <hess_michael@bah.com>
2017-12-14 13:39:38 -0500

    Fix update query

--- servicedesk/deactivate_old_users.sql
+++ servicedesk/deactivate_old_users.sql
@@ -6,8 +6,9 @@ SELECT COUNT(*)
   WHERE cwd_user.active = 1;
 
 UPDATE cwd_user
-  SET cwd_user.active = 0
-  JOIN cwd_user_attributes ON cwd_user_attributes.user_id = cwd_user.id
-                           AND cwd_user_attributes.attribute_name = 'login.lastLoginMillis'
-                           AND cwd_user_attributes.attribute_value <= CAST((extract(epoch from now()) - 10368000) * 1000 AS text)
-  WHERE cwd_user.active = 1;
+  SET active = 0
+  WHERE id IN (SELECT cwd_user_attributes.user_id
+                 FROM cwd_user_attributes
+                 WHERE cwd_user_attributes.attribute_name = 'login.lastLoginMillis'
+                 AND cwd_user_attributes.attribute_value <= CAST((extract(epoch from now()) - 10368000) * 1000 AS text))
+  AND active = 1;
