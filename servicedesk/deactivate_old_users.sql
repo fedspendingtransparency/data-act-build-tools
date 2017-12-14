5e27154ff5e0d8582ff85d6958a4d79306519e36
Michael Hess <hess_michael@bah.com>
2017-12-14 13:18:33 -0500

    Fix query and add count

--- servicedesk/deactivate_old_users.sql
+++ servicedesk/deactivate_old_users.sql
@@ -1,6 +1,13 @@
+SELECT COUNT(*)
+  FROM cwd_user
+  JOIN cwd_user_attributes ON cwd_user_attributes.user_id = cwd_user.id
+                           AND cwd_user_attributes.attribute_name = 'login.lastLoginMillis'
+                           AND cwd_user_attributes.attribute_value <= CAST((extract(epoch from now()) - 10368000) * 1000 AS text)
+  WHERE cwd_user.active = 1;
+
 UPDATE cwd_user
   SET cwd_user.active = 0
   JOIN cwd_user_attributes ON cwd_user_attributes.user_id = cwd_user.id
                            AND cwd_user_attributes.attribute_name = 'login.lastLoginMillis'
-                           AND cwd_user_attributes.attribute_value < ((extract(epoch from now())  - 10368000) * 1000)
+                           AND cwd_user_attributes.attribute_value <= CAST((extract(epoch from now()) - 10368000) * 1000 AS text)
   WHERE cwd_user.active = 1;
