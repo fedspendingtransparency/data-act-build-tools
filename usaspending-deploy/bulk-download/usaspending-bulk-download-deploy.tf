bb2a236021eb5828fc13ed1434fcaad44c77b774
Katie Rose <rose_katherine@bah.com>
2018-01-02 16:33:48 -0500

    Updating var names for Bulk Downloader and adding tags

--- usaspending-deploy/bulk-download/usaspending-bulk-download-deploy.tf
+++ usaspending-deploy/bulk-download/usaspending-bulk-download-deploy.tf
@@ -2,34 +2,56 @@ provider "aws" {
     region = "${var.aws_region}"
 }
 
-resource "aws_autoscaling_group" "api-asg" {
-  name = "${var.api_name_prefix}_ASG_${lookup(var.aws_amis, var.aws_region)}"
-  max_size = "${var.api_asg_max}"
-  min_size = "${var.api_asg_min}"
-  desired_capacity = "${var.api_asg_desired}"
-  min_elb_capacity = "${var.api_asg_min}"
+resource "aws_autoscaling_group" "bd-asg" {
+  name = "${var.bd_name_prefix}_ASG_${lookup(var.aws_amis, var.aws_region)}"
+  max_size = "${var.bd_asg_max}"
+  min_size = "${var.bd_asg_min}"
+  desired_capacity = "${var.bd_asg_desired}"
+  min_elb_capacity = "${var.bd_asg_min}"
   health_check_type = "ELB"
   health_check_grace_period = 180
-  launch_configuration = "${aws_launch_configuration.api-lc.name}"
-  load_balancers = ["${var.api_elb}"]
+  launch_configuration = "${aws_launch_configuration.bd-lc.name}"
+  load_balancers = ["${var.bd_elb}"]
   vpc_zone_identifier = ["${split(",", var.subnets)}"]
-  tag {
-    key = "Name"
-    value = "${var.api_name_prefix}_ASG"
-    propagate_at_launch = "true"
-  }
+  tags = [
+    {
+      key = "Name"
+      value = "${var.bd_name_prefix}_ASG"
+      propagate_at_launch = "true"
+    },
+    {
+      key = "Application"
+      value = "USAspending"
+      propagate_at_launch = "true"
+    },
+    {
+      key = "Component"
+      value = "BulkDownload"
+      propagate_at_launch = "true"
+    },
+    {
+      key = "Environment"
+      value = "${var.bd_env_tag}"
+      propagate_at_launch = "true"
+    },
+    {
+      key = "DeployTime"
+      value = "${timestamp()}"
+      propagate_at_launch = "true"
+    }
+  ]
   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_launch_configuration" "api-lc" {
-  name = "${var.api_name_prefix}_LC_${lookup(var.aws_amis, var.aws_region)}"
+resource "aws_launch_configuration" "bd-lc" {
+  name = "${var.bd_name_prefix}_LC_${lookup(var.aws_amis, var.aws_region)}"
   image_id = "${lookup(var.aws_amis, var.aws_region)}"
-  instance_type = "${var.api_instance_type}"
+  instance_type = "${var.bd_instance_type}"
   ebs_optimized = true
-  iam_instance_profile = "${var.api_iam_profile}"
-  security_groups = ["${split(",", var.api_sec_groups)}"]
+  iam_instance_profile = "${var.bd_iam_profile}"
+  security_groups = ["${split(",", var.bd_sec_groups)}"]
   user_data = "${var.user_data}"
   key_name = "${var.key_name}"
   lifecycle {
@@ -37,17 +59,17 @@ resource "aws_launch_configuration" "api-lc" {
   }
 }
 
-resource "aws_autoscaling_policy" "api_scale_up" {
-  name                   = "${var.api_name_prefix}_ScaleUp"
+resource "aws_autoscaling_policy" "bd_scale_up" {
+  name                   = "${var.bd_name_prefix}_ScaleUp"
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 300
   policy_type            = "SimpleScaling"
-  autoscaling_group_name = "${aws_autoscaling_group.api-asg.name}"
+  autoscaling_group_name = "${aws_autoscaling_group.bd-asg.name}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "api_alarm_high_cpu" {
-  alarm_name          = "${var.api_name_prefix}_HighCPU"
+resource "aws_cloudwatch_metric_alarm" "bd_alarm_high_cpu" {
+  alarm_name          = "${var.bd_name_prefix}_HighCPU"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -57,24 +79,24 @@ resource "aws_cloudwatch_metric_alarm" "api_alarm_high_cpu" {
   threshold           = "50"
 
   dimensions {
-    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
+    AutoScalingGroupName = "${aws_autoscaling_group.bd-asg.name}"
   }
 
-  alarm_description = "High CPU on ${var.api_name_prefix}"
-  alarm_actions     = ["${aws_autoscaling_policy.api_scale_up.arn}"]
+  alarm_description = "High CPU on ${var.bd_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.bd_scale_up.arn}"]
 }
 
-resource "aws_autoscaling_policy" "api_scale_down" {
-  name                   = "${var.api_name_prefix}_ScaleDown"
+resource "aws_autoscaling_policy" "bd_scale_down" {
+  name                   = "${var.bd_name_prefix}_ScaleDown"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
   cooldown               = 30
   policy_type            = "SimpleScaling"
-  autoscaling_group_name = "${aws_autoscaling_group.api-asg.name}"
+  autoscaling_group_name = "${aws_autoscaling_group.bd-asg.name}"
 }
 
-resource "aws_cloudwatch_metric_alarm" "api_alarm_low_cpu" {
-  alarm_name          = "${var.api_name_prefix}_LowCPU"
+resource "aws_cloudwatch_metric_alarm" "bd_alarm_low_cpu" {
+  alarm_name          = "${var.bd_name_prefix}_LowCPU"
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
@@ -84,9 +106,9 @@ resource "aws_cloudwatch_metric_alarm" "api_alarm_low_cpu" {
   threshold           = "5"
 
   dimensions {
-    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
+    AutoScalingGroupName = "${aws_autoscaling_group.bd-asg.name}"
   }
 
-  alarm_description = "All Instance CPU low ${var.api_name_prefix}"
-  alarm_actions     = ["${aws_autoscaling_policy.api_scale_down.arn}"]
+  alarm_description = "All Instance CPU low ${var.bd_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.bd_scale_down.arn}"]
 }
