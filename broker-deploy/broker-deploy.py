b18872ee466ba74f1a2b679b564ad36edd4fb6ff
klrBAH <rose_katherine@bah.com>
2018-01-29 17:24:37 -0500

    Adding dev and (partially) sandbox to broker deploy

---
+++ broker-deploy/broker-deploy.py
@@ -0,0 +1,176 @@
+import boto
+from boto import ec2
+import sh
+import os
+import json
+import argparse
+import sys
+
+def deploy():
+
+    # set paths
+    packer_file = 'broker-packer.json'
+    tfvar_file = 'broker-vars.tf.json'
+    packer_exec_path = '/packer/packerio'
+    tf_exec_path = '/terraform/terraform'
+
+    #Set connection
+    print('Connecting to AWS via region us-gov-west-1...')
+    conn = boto.ec2.connect_to_region(region_name='us-gov-west-1')
+    print('Done.')
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-sbx", "--sandbox", action="store_true", help="Runs deploy for sandbox")
+    parser.add_argument("-dev", "--dev", action="store_true", help="Runs deploy for dev")
+    parser.add_argument("-stg", "--staging", action="store_true", help="Runs deploy for staging")
+    parser.add_argument("-prod", "--prod", action="store_true", help="Runs deploy for prod")
+    args = parser.parse_args()
+    optionsDict = vars(args)
+    noArgs = True
+
+    for arg in optionsDict:
+        if(optionsDict[arg]):
+            noArgs = False
+
+    if noArgs:
+        print ("No environment specified. Please include an argument: --sandbox, --dev, --staging, --prod")
+        sys.exit(1)
+
+    if optionsDict["sandbox"]:
+        deploy_env = 'sandbox'
+
+    if optionsDict["dev"]:
+        deploy_env = 'dev'
+
+    if optionsDict["staging"]:
+        deploy_env = 'staging'
+
+    if optionsDict["sandbox"] or optionsDict["dev"]:
+
+        # Retrieve current Base app AMI (where type=app and current=true)
+        print('Retrieving current base app AMI...')
+        current_base_ami = conn.get_all_images(filters={
+            "tag:current" : "True",
+            "tag:base" : "True",
+            "tag:type" : "Application"
+            })[0].id
+        print('Done. Current base app AMI: ' + current_base_ami)
+
+        # Insert current Base app AMI into packer file
+        print('Updating Packer file with current base app AMI ' + current_base_ami + '...')
+        update_packer_spec(packer_file, current_base_ami)
+        print('Done.')
+
+        # Retrieve current App Instance AMIs
+        print('Retrieving current app instance AMI(s)...')
+        current_app_amis = conn.get_all_images(filters={
+            "tag:current" : "True",
+            "tag:base" : "False",
+            "tag:type" : "Application",
+            "tag:environment" : deploy_env
+            })
+        print('Done. Current app instance AMI(s): '+ '\n'.join(map(str, current_app_amis)) )
+
+        # Build new app instance AMI via Packer
+        print('**************************************************************************')
+        print('Buiding new app instance AMI via Packer. This may take a few minutes...')
+        packer_output = packer_build(packer_file, packer_exec_path)
+        ami_line = [line for line in packer_output.split('\n') if "amazon-ebs: AMIs were created:" in line][0]
+        ami_id = ami_line[ami_line.find('ami-'):ami_line.find('ami-')+12]
+        print('Done. Packer AMI created: '+ami_id)
+
+        # Set current=False tag for old App AMIs
+        if current_app_amis:
+            print('Setting current tag to False on old instance AMIs: \n' + '\n'.join(map(str, current_app_amis)) )
+            update_ami_tags(current_app_amis)
+            print('Done.')
+        else:
+            print('No matching old AMIs. Skipping tag update...')
+
+        # Confirm app instance that was created is now only current=True tagged AMI
+        if ami_id == conn.get_all_images(filters={
+            "tag:current" : "True",
+            "tag:base" : "False",
+            "tag:type" : "Application",
+            "tag:environment" : deploy_env
+            })[0].id:
+            print('Success! Packer AMI id matches current tagged AMI.')
+        else:
+            print('Something went wrong. Packer AMI: '+ami_id+'; Tagged AMI: '+conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "False", "tag:type" : "Application", "tag:environment" : deploy_env})[0].id)
+
+        #Add new AMI id to terraform variables
+        update_lc_ami(ami_id, tfvar_file)
+
+        #Run terraform
+        run_tf(tf_exec_path)
+
+    elif optionsDict["prod"]:
+        #For prod, we don't build a new artifact, we just run terraform against the current staging AMI
+        deploy_env = 'prod'
+        # get current staging AMI
+        staging_ami = conn.get_all_images(filters={"tag:current" : "True", "tag:base" : "False", "tag:type" : "Application", "tag:environment" : "staging"})[0].id
+        # Update terraform variables with staging ami_id
+        update_lc_ami(staging_ami, 'variables.tf.json')
+        print(staging_ami)
+        #Rune terraform
+        run_tf(tf_exec_path)
+
+
+def run_tf(tf_exec_path):
+    # Run terraform
+    plan_output = tf_plan(tf_exec_path)
+    print(plan_output)
+    apply_output = tf_apply(tf_exec_path)
+    print(apply_output)
+    return
+
+
+def packer_build(packer_file='packer.json', packer_exec_path='packer'):
+    cmd = sh.Command(packer_exec_path).build.bake(packer_file).bake('-machine-readable')
+    return cmd()
+
+def tf_plan(tf_exec_path):
+    cmd = sh.Command(tf_exec_path).plan
+    return cmd()
+
+def tf_apply(tf_exec_path):
+    cmd = sh.Command(tf_exec_path).apply
+    return cmd()
+
+def update_packer_spec(packer_file='packer.json', current_base_ami=''):
+    packer_json = open(packer_file, "r")
+    packer_data = json.load(packer_json)
+    packer_json.close()
+
+    packer_data['builders'][0]['source_ami'] = current_base_ami
+
+    packer_json = open(packer_file, "w+")
+    packer_json.write(json.dumps(packer_data))
+    packer_json.close()
+
+    return
+
+def update_lc_ami(new_ami='', tfvar_file='variables.tf.json'):
+    tfvar_json = open(tfvar_file, "r")
+    tfvar_data = json.load(tfvar_json)
+    tfvar_json.close()
+
+    tfvar_data['variable']['aws_amis']['default']['us-gov-west-1'] = new_ami
+
+    tfvar_json = open(tfvar_file, "w+")
+    tfvar_json.write(json.dumps(tfvar_data))
+    tfvar_json.close()
+
+    return
+
+def update_ami_tags(current_app_amis=False):
+    for ami in current_app_amis:
+        ami.remove_tag('current','True')
+        ami.add_tag('current','False')
+
+    return
+
+
+if __name__ == '__main__':
+    deploy()
---
+++ broker-deploy/broker-deploy.tf
@@ -0,0 +1,218 @@
+# Terraform deployment file
+# Separate variables definitions required
+
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+resource "aws_autoscaling_group" "api-asg" {
+  name = "${aws_launch_configuration.api-lc.name}"
+  max_size = "${var.api_asg_max}"
+  min_size = "${var.api_asg_min}"
+  desired_capacity = "${var.api_asg_desired}"
+  min_elb_capacity = "${var.api_asg_min}"
+  health_check_type = "ELB"
+  health_check_grace_period = 180
+  launch_configuration = "${aws_launch_configuration.api-lc.name}"
+  load_balancers = ["${var.api_elb}"]
+  vpc_zone_identifier = ["${split(",", var.subnets)}"]
+    
+  tag = {
+      key = "Name"
+      value = "${var.api_name_prefix}_${lookup(var.aws_amis, var.aws_region)}"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Application"
+      value = "Broker"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Component"
+      value = "API"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Environment"
+      value = "${var.env_tag}"
+      propagate_at_launch = "true"
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "val-asg" {
+  name = "${aws_launch_configuration.val-lc.name}"
+  max_size = "${var.val_asg_max}"
+  min_size = "${var.val_asg_min}"
+  desired_capacity = "${var.val_asg_desired}"
+  min_elb_capacity = "${var.val_asg_min}"
+  health_check_type = "ELB"
+  health_check_grace_period = 180
+  launch_configuration = "${aws_launch_configuration.val-lc.name}"
+  load_balancers = ["${var.val_elb}"]
+  vpc_zone_identifier = ["${split(",", var.subnets)}"]
+    
+  tag = {
+      key = "Name"
+      value = "${var.val_name_prefix}_${lookup(var.aws_amis, var.aws_region)}"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Application"
+      value = "Broker"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Component"
+      value = "Validator"
+      propagate_at_launch = "true"
+  }
+  tag = {
+      key = "Environment"
+      value = "${var.env_tag}"
+      propagate_at_launch = "true"
+  }
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "api-lc" {
+  name = "${var.api_name_prefix}_${lookup(var.aws_amis, var.aws_region)}"
+  image_id = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.api_instance_type}"
+  iam_instance_profile = "${var.api_iam_profile}"
+  # Security group
+  security_groups = ["${split(",", var.api_sec_groups)}"]
+  user_data="${var.api_user_data}"
+  key_name = "${var.key_name}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_launch_configuration" "val-lc" {
+  name = "${var.val_name_prefix}_${lookup(var.aws_amis, var.aws_region)}"
+  image_id = "${lookup(var.aws_amis, var.aws_region)}"
+  instance_type = "${var.val_instance_type}"
+  iam_instance_profile = "${var.val_iam_profile}"
+  # Security group
+  security_groups = ["${split(",", var.val_sec_groups)}"]
+  user_data="${var.val_user_data}"
+  key_name = "${var.key_name}"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_policy" "val_scale_up" {
+  name                   = "${var.val_name_prefix}_ScaleUp"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = "${aws_autoscaling_group.val-asg.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "val_alarm_high_cpu" {
+  alarm_name          = "${var.val_name_prefix}_HighCPU75"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "75"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.val-asg.name}"
+  }
+
+  alarm_description = "High CPU over 75% on ${var.val_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.val_scale_up.arn}"]
+}
+
+resource "aws_autoscaling_policy" "val_scale_down" {
+  name                   = "${var.val_name_prefix}_ScaleDown"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 30
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = "${aws_autoscaling_group.val-asg.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "val_alarm_low_cpu" {
+  alarm_name          = "${var.val_name_prefix}_LowCPU5"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "5"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.val-asg.name}"
+  }
+
+  alarm_description = "All Instance CPU below 5% on ${var.val_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.val_scale_down.arn}"]
+}
+
+resource "aws_autoscaling_policy" "api_scale_up" {
+  name                   = "${var.api_name_prefix}_ScaleUp"
+  scaling_adjustment     = 1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 300
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = "${aws_autoscaling_group.api-asg.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_alarm_high_cpu" {
+  alarm_name          = "${var.api_name_prefix}_HighCPU75"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = "75"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
+  }
+
+  alarm_description = "High CPU over 75% on ${var.api_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.api_scale_up.arn}"]
+}
+
+resource "aws_autoscaling_policy" "api_scale_down" {
+  name                   = "${var.api_name_prefix}_ScaleDown"
+  scaling_adjustment     = -1
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 30
+  policy_type            = "SimpleScaling"
+  autoscaling_group_name = "${aws_autoscaling_group.api-asg.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "api_alarm_low_cpu" {
+  alarm_name          = "${var.api_name_prefix}_LowCPU5"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = "5"
+
+  dimensions {
+    AutoScalingGroupName = "${aws_autoscaling_group.api-asg.name}"
+  }
+
+  alarm_description = "All Instance CPU below 5% on ${var.api_name_prefix}"
+  alarm_actions     = ["${aws_autoscaling_policy.api_scale_down.arn}"]
+}
