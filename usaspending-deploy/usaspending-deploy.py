8dec098a240380f7fed392a03da11202443cf3ec
Hitesh Sharma <33040859+hsharma96@users.noreply.github.com>
2018-07-11 16:55:02 -0400

    updating terraform apply/plan flags

--- usaspending-deploy/usaspending-deploy.py
+++ usaspending-deploy/usaspending-deploy.py
@@ -116,8 +116,8 @@ def deploy():
         update_terraform_user_data(deploy_env)    
 
         # Run Terraform plan and apply
-        real_time_command([tf_exec_path, 'plan'])
-        real_time_command([tf_exec_path, 'apply'])
+        real_time_command([tf_exec_path, 'plan', "--input=false"])
+        real_time_command([tf_exec_path, 'apply', "--input=false", "--auto-approve"])
 
   ###########################
   #   TF Build - Prod       #
@@ -140,8 +140,8 @@ def deploy():
         update_terraform_user_data('prod')  
 
         # Run Terraform plan and apply
-        real_time_command([tf_exec_path, 'plan', '-input=false'])
-        real_time_command([tf_exec_path, 'apply', '-input=false', '-auto-approve'])
+        real_time_command([tf_exec_path, 'plan', '--input=false'])
+        real_time_command([tf_exec_path, 'apply', '--input=false', '--auto-approve'])
 
     global EXIT_CODE
     if EXIT_CODE != 0:
