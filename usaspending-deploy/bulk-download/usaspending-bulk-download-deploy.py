9da9668651ad43a9af431d8fe970325a6046c52f
Hitesh Sharma <33040859+hsharma96@users.noreply.github.com>
2018-07-11 17:08:13 -0400

    adding apply/plan flags

--- usaspending-deploy/bulk-download/usaspending-bulk-download-deploy.py
+++ usaspending-deploy/bulk-download/usaspending-bulk-download-deploy.py
@@ -12,7 +12,7 @@ def deploy():
     # This tf_var file is expected to be copied from an external source
     tfvar_file   = 'usaspending-bulk-download-vars.tf.json'
 
-    tf_exec_path = '/opt/terraform'
+    tf_exec_path = '/opt/terraform/terraform'
     tf_file      = 'usaspending-deploy.tf'
 
     # Set connection
@@ -67,8 +67,8 @@ def deploy():
     update_tf_ami(current_api_ami, tfvar_file)
 
     # Run Terraform plan and apply
-    real_time_command([tf_exec_path, 'plan'])
-    real_time_command([tf_exec_path, 'apply'])
+    real_time_command([tf_exec_path, 'plan', "--input=false"])
+    real_time_command([tf_exec_path, 'apply', "--input=false", "--auto-approve"])
 
     global EXIT_CODE
     if EXIT_CODE != 0:
