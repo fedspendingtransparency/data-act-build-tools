94415eb17096789d170b4085536c4df7415f4ab1
myazidi <alaouiyazidi_marouane@bah.com>
2019-05-15 15:41:12 -0400

    Adding profile and elbname arg

--- servicedesk-scripts/reboot_Instance.py
+++ servicedesk-scripts/reboot_Instance.py
@@ -1,23 +1,28 @@
 import boto3
 import time
 import sys
-import logging
+import argparse
 
 
 region = 'us-east-1'
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--elbname', required=True, type=str)
+args = parser.parse_args()
+elbname = args.elbname
+
 def main():
-    session = boto3.Session(profile_name='marouane')
+    session = boto3.Session(profile_name='cf-invalidation')
 
     elb = session.client('elb', region_name=region)
     instance = elb.describe_load_balancers(LoadBalancerNames=[
-        'servicedesk-dev',
+        elbname,
     ])['LoadBalancerDescriptions'][0]['Instances'][0]['InstanceId']
     ec2 = session.client('ec2', region_name=region)
     ec2.reboot_instances(InstanceIds=[instance, ])
     print('instance rebooted with Instance ID: ' + instance)
     elb_check = elb.describe_instance_health(
-        LoadBalancerName='servicedesk-dev',
+        LoadBalancerName=elbname,
     )
     # Wait for the ELB health check to turn OutOfService
     time.sleep(60)
