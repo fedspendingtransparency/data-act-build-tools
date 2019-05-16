7953ac2b90a9ead22539330e3a24de45f5a57f3c
myazidi <alaouiyazidi_marouane@bah.com>
2019-05-15 21:59:04 -0400

    Final Changes to reboot_Instance.py

--- servicedesk-scripts/reboot_Instance.py
+++ servicedesk-scripts/reboot_Instance.py
@@ -15,20 +15,23 @@ def main():
     session = boto3.Session(profile_name='cf-invalidation')
 
     elb = session.client('elb', region_name=region)
-    instance = elb.describe_load_balancers(LoadBalancerNames=[
-        elbname,
-    ])['LoadBalancerDescriptions'][0]['Instances'][0]['InstanceId']
+
+    instance = elb.describe_load_balancers(LoadBalancerNames=[elbname,])['LoadBalancerDescriptions'][0]['Instances'][0]['InstanceId']
+    
     ec2 = session.client('ec2', region_name=region)
     ec2.reboot_instances(InstanceIds=[instance, ])
-    print('instance rebooted with Instance ID: ' + instance)
+
+    print("Instance with Instance ID: {} is rebooted".format(instance))
     
-    # Wait for the ELB health check to turn OutOfService
+    # Wait for the ELB health check to go OutOfService
+    
     time.sleep(60)
 
     minutes  = 0
     maxtime = 10
     status = None
-    # wait fir the health check to become InService
+   
+   # wait for the health check to become InService
 
     while (minutes < maxtime ):
 
@@ -38,6 +41,7 @@ def main():
             break
         time.sleep(60)
         minutes += 1
+        
         print("The instance status is {} after {} minute(s)".format (status, minutes))
 
     if (status == "OutOfService" and minutes == maxtime):
