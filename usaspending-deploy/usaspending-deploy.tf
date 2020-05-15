e8dae00cf1463857a273227c67098d7f99b8ae6d
Ben Minahan <64608730+bminahankcfrb@users.noreply.github.com>
2020-05-15 13:38:04 -0500

    fixing the cooldown

--- usaspending-deploy/usaspending-deploy.tf
+++ usaspending-deploy/usaspending-deploy.tf
@@ -76,7 +76,6 @@ resource "aws_cloudwatch_metric_alarm" "api_alarm_high_requests" {
   alarm_name          = "${var.api_name_prefix}_requestshigh (${var.aws_amis[var.aws_region]})"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "1"
-  cooldown            = "300"
   threshold           = "5000"
 
   metric_query {
@@ -126,7 +125,7 @@ resource "aws_autoscaling_policy" "api_scale_down" {
   name                   = "${var.api_name_prefix}_scaledown (${var.aws_amis[var.aws_region]})"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
-  cooldown               = 30
+  cooldown               = 300
   policy_type            = "SimpleScaling"
   autoscaling_group_name = aws_autoscaling_group.api_asg.name
 }
@@ -136,7 +135,6 @@ resource "aws_cloudwatch_metric_alarm" "api_alarm_low_requests" {
   comparison_operator = "LessThanOrEqualToThreshold"
   evaluation_periods  = "2"
   threshold           = "5000"
-  cooldown            = "300"
 
   metric_query {
     id          = "e1"
