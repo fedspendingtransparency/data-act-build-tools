49351cb014d1ca45f86e662fa61eed4ba1d1ad14
Ben Minahan <64608730+bminahankcfrb@users.noreply.github.com>
2020-07-09 11:05:02 -0500

    getting rid of iops provisioning

--- broker-deploy/broker-deploy.tf
+++ broker-deploy/broker-deploy.tf
@@ -105,7 +105,6 @@ resource "aws_launch_configuration" "api-lc" {
   root_block_device {
     volume_size = var.api_ebs_size
     volume_type = var.api_ebs_type
-    iops        = var.api_ebs_iops
   }
 }
 
@@ -125,7 +124,6 @@ resource "aws_launch_configuration" "val-lc" {
   root_block_device {
     volume_size = var.val_ebs_size
     volume_type = var.val_ebs_type
-    iops        = var.val_ebs_iops
   }
 }
 
