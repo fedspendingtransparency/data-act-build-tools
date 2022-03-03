4ecad5afd3d016b06e4f0bb9720123877101e059
Danny Martin <martin_daniel@bah.com>
2022-03-03 16:42:59 -0500

    testing python output with link

--- databricks/databricks-jobs.py
+++ databricks/databricks-jobs.py
@@ -3,6 +3,7 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
 
+# REMOVE WHEN SSL IS ENABLED
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 INSTANCE_ID = sys.argv[1]
@@ -41,10 +42,20 @@ def getJobIds(res):
       tempDict[job['settings']['name']] = job['job_id']
     return tempDict
 
+def link(uri, label=None):
+    if label is None: 
+        label = uri
+    parameters = ''
+
+    # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
+    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+
+    return escape_mask.format(parameters, uri, label)
+
 jobs = getJobIds(getRequest('/jobs/list'))
 
 if( JOB_NAME in jobs ):
-    print (jobs[JOB_NAME])
+    print("JOB ID: " + jobs[JOB_NAME])
 
     job_params = {'job_id': jobs[JOB_NAME]}
     startJob = postRequest('/jobs/run-now', job_params)
@@ -52,16 +63,16 @@ if( JOB_NAME in jobs ):
     run_id = startJob.json()['run_id']
     run_params = { 'run_id' : run_id }
     job_status = getRequest('/jobs/runs/get-output', run_params).json()["metadata"]["state"]["life_cycle_state"]
-    print(job_status)
 
     #Wait for job to finish running
     while(job_status == "RUNNING" or job_status == "PENDING"):
         job_status = getRequest('/jobs/runs/get-output', run_params).json()["metadata"]["state"]["life_cycle_state"]
+        print("JOB IS RUNNING")
 
     finishedJob = getRequest('/jobs/runs/get-output', run_params)
     print(json.dumps(json.loads(finishedJob.text), indent = 2))
 
     run_url = finishedJob.json()["metadata"]["run_page_url"].replace("webapp", INSTANCE_ID+"/")
-    print("---------------SEE JOB RUN HERE: " + run_url)
+    print("---------------SEE JOB RUN HERE: " + link(run_url))
 else:
     raise ValueError(sys.argv[2] + " is not a job in databricks")
