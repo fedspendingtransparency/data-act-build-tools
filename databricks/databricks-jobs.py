7cddfee98c79bf21e3d64aecf5a5ecd9b16f9387
Danny Martin <martin_daniel@bah.com>
2022-06-13 14:56:32 -0400

    add job parameters

--- databricks/databricks-jobs.py
+++ databricks/databricks-jobs.py
@@ -5,15 +5,16 @@ import json
 
 INSTANCE_ID = sys.argv[1]
 JOB_NAME = sys.argv[2]
-API_VERSION = '/api/2.1'
+JOB_PARAMETERS = sys.argv[3]
+API_VERSION = "/api/2.1"
 
 print("----------RUNNING JOB " + JOB_NAME )
 
 # Run Get request with api_command param
 # /jobs/list/ with api 2.0 returns all jobs, 2.1 does not
 def getRequest(api_command, params={}):
-    if api_command == '/jobs/list':
-        url = "https://{}{}{}".format(INSTANCE_ID, '/api/2.0', api_command)
+    if api_command == "/jobs/list":
+        url = "https://{}{}{}".format(INSTANCE_ID, "/api/2.0", api_command)
     else:
         url = "https://{}{}{}".format(INSTANCE_ID, API_VERSION, api_command)
     response = requests.get(
@@ -31,30 +32,30 @@ def postRequest(api_command, params):
     )
     return response
 
-# Get all job names and jobID's and map to dict
+# Get all job names and jobID"s and map to dict
 def getJobIds(res):
     tempDict = {}
-    for job in res.json()['jobs']:
-      tempDict[job['settings']['name']] = job['job_id']
+    for job in res.json()["jobs"]:
+      tempDict[job["settings"]["name"]] = job["job_id"]
     return tempDict
 
-jobs = getJobIds(getRequest('/jobs/list'))
+jobs = getJobIds(getRequest("/jobs/list"))
 
 if( JOB_NAME in jobs ):
     print("JOB ID: " + str(jobs[JOB_NAME]))
 
-    job_params = {'job_id': jobs[JOB_NAME]}
-    startJob = postRequest('/jobs/run-now', job_params)
+    job_params = { "job_id": jobs[JOB_NAME], "notebook_params": JOB_PARAMETERS }
+    startJob = postRequest("/jobs/run-now", job_params)
 
-    run_id = startJob.json()['run_id']
-    run_params = { 'run_id' : run_id }
-    job_status = getRequest('/jobs/runs/get', run_params).json()["state"]["life_cycle_state"]
+    run_id = startJob.json()["run_id"]
+    run_params = { "run_id" : run_id }
+    job_status = getRequest("/jobs/runs/get", run_params).json()["state"]["life_cycle_state"]
 
     #Wait for job to finish running
     while(job_status == "RUNNING" or job_status == "PENDING"):
-        job_status = getRequest('/jobs/runs/get', run_params).json()["state"]["life_cycle_state"]
+        job_status = getRequest("/jobs/runs/get", run_params).json()["state"]["life_cycle_state"]
 
-    tasks = getRequest('/jobs/runs/get', run_params).json()["tasks"]
+    tasks = getRequest("/jobs/runs/get", run_params).json()["tasks"]
 
     # Get all run ids for each task in the job
     all_run_ids = []
@@ -62,8 +63,8 @@ if( JOB_NAME in jobs ):
         all_run_ids.append(x["run_id"])
 
     for run in all_run_ids:
-        run_params = {'run_id' : run}
-        finishedJob = getRequest('/jobs/runs/get-output', run_params)
+        run_params = {"run_id" : run}
+        finishedJob = getRequest("/jobs/runs/get-output", run_params)
         print(json.dumps(json.loads(finishedJob.text), indent = 2))
 
     run_url = finishedJob.json()["metadata"]["run_page_url"].replace("webapp", INSTANCE_ID+"/")
