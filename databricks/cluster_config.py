73b8df0b81676232029648347f696f96f6b76fca
Danny Martin <martin_daniel@bah.com>
2022-06-22 17:09:47 -0400

    update ternary

--- databricks/cluster_config.py
+++ databricks/cluster_config.py
@@ -36,9 +36,9 @@ def updateJsonFile(fileName):
     data["tasks"][0]["spark_python_task"]["python_file"] = "dbfs:/FileStore/" + BRANCH + "/manage.py"
     data["tasks"][0]["spark_python_task"]["parameters"] = python_params
     data["tasks"][0]["new_cluster"]["spark_env_vars"] = env_vars
-    data["tasks"][0]["node_type_id"] = data["tasks"][0]["node_type_id"] == "" ? "m5a.large" : NODE_TYPE
-    data["tasks"][0]["driver_node_type_id"] = data["tasks"][0]["driver_node_type_id"] == "" ? "m5a.large" : NODE_TYPE
-    data["tasks"][0]["num_workers"] = data["tasks"][0]["num_workers"] == "" ? 0 : int(WORKERS)
+    data["tasks"][0]["node_type_id"] = "m5a.large" if data["tasks"][0]["node_type_id"] == "" else NODE_TYPE
+    data["tasks"][0]["driver_node_type_id"] = "m5a.large" if data["tasks"][0]["driver_node_type_id"] == "" else NODE_TYPE
+    data["tasks"][0]["num_workers"] = 0 if data["tasks"][0]["num_workers"] == "" else int(WORKERS)
     data["name"] = JOB_NAME
 
     ## Save our changes to JSON file
