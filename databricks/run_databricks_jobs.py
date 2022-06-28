41b7dc6d903dac1c290a9fb5af8d86baaf12decf
Danny Martin <martin_daniel@bah.com>
2022-06-28 10:41:25 -0400

    add wait boolean

--- databricks/run_databricks_jobs.py
+++ databricks/run_databricks_jobs.py
@@ -3,10 +3,16 @@ import requests
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import json
 
+# TODO implement argparse
+
 INSTANCE_ID = sys.argv[1]
 JOB_NAME = sys.argv[2]
 JOB_PARAMETERS = sys.argv[3]
 API_VERSION = "/api/2.1"
+try:
+    WAIT_BOOLEAN = sys.argv[4]
+except:
+    WAIT_BOOLEAN = True
 
 # Run Get request with api_command param
 # /jobs/list/ with api 2.0 returns all jobs, 2.1 does not
@@ -64,29 +70,31 @@ if __name__ == '__main__':
         run_id = startJob.json()["run_id"]
         run_params = { "run_id" : run_id }
         job_status = getRequest("/jobs/runs/get", run_params).json()["state"]["life_cycle_state"]
+        if WAIT_BOOLEAN:
+            # Wait for job to finish running
+            while(job_status == "RUNNING" or job_status == "PENDING"):
+                job_status = getRequest("/jobs/runs/get", run_params).json()["state"]["life_cycle_state"]
 
-        # Wait for job to finish running
-        while(job_status == "RUNNING" or job_status == "PENDING"):
-            job_status = getRequest("/jobs/runs/get", run_params).json()["state"]["life_cycle_state"]
+            # Error out if the job has not succeeded
+            job_status_done = getRequest("/jobs/runs/get", run_params).json()
+            if(job_status_done["state"]["result_state"] != "SUCCESS"):
+                raise Exception("Job did not succeed - url: https://" + INSTANCE_ID + job_status_done["run_page_url"]) 
 
-        # Error out if the job has not succeeded
-        job_status_done = getRequest("/jobs/runs/get", run_params).json()
-        if(job_status_done["state"]["result_state"] != "SUCCESS"):
-            raise Exception("Job did not succeed - url: https://" + INSTANCE_ID + job_status_done["run_page_url"]) 
+            tasks = getRequest("/jobs/runs/get", run_params).json()["tasks"]
 
-        tasks = getRequest("/jobs/runs/get", run_params).json()["tasks"]
+            # Get all run ids for each task in the job
+            all_run_ids = []
+            for x in tasks:
+                all_run_ids.append(x["run_id"])
 
-        # Get all run ids for each task in the job
-        all_run_ids = []
-        for x in tasks:
-            all_run_ids.append(x["run_id"])
-
-        for run in all_run_ids:
-            run_params = {"run_id" : run}
-            finishedJob = getRequest("/jobs/runs/get-output", run_params)
-            print(json.dumps(json.loads(finishedJob.text), indent = 2))
-            run_url = finishedJob.json()["metadata"]["run_page_url"].replace("webapp", INSTANCE_ID+"/")
-            print("---------------SEE JOB RUN HERE: " + "https://" + run_url)
+            for run in all_run_ids:
+                run_params = {"run_id" : run}
+                finishedJob = getRequest("/jobs/runs/get-output", run_params)
+                print(json.dumps(json.loads(finishedJob.text), indent = 2))
+                run_url = finishedJob.json()["metadata"]["run_page_url"].replace("webapp", INSTANCE_ID+"/")
+                print("---------------SEE JOB RUN HERE: " + "https://" + run_url)
+        else:
+            print(json.dumps(json.loads(startJob.text), indent = 2))
         
     else:
         raise ValueError(sys.argv[2] + " is not a job in databricks")
