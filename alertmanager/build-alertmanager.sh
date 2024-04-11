f1b40eb4fb9899a987382e6be7d0d7a64b5645db
RYLIE NICHOL <Rylie.Nichol@kc.frb.org>
2024-04-11 10:01:07 -0500

    blackbox,prometheus,alertmanager images

--- alertmanager/build-alertmanager.sh
+++ alertmanager/build-alertmanager.sh
@@ -5,5 +5,5 @@ ACCOUNT=${1}
 
 ./template_config.sh feature_template.yml
 
-docker build -t $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/alertmanager:$BRANCH -f Dockerfile .
-docker push $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/alertmanager:$BRANCH
+docker build -t $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/alertmanager:latest -f Dockerfile .
+docker push $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/alertmanager:latest
--- blackbox/build-blackbox.sh
+++ blackbox/build-blackbox.sh
@@ -3,5 +3,5 @@
 BRANCH=${GIT_BRANCH:-"$(git branch --show-current)"}
 ACCOUNT=${1}
 
-docker build -t $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/blackbox:$BRANCH -f Dockerfile .
-docker push $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/blackbox:$BRANCH
+docker build -t $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/blackbox:latest -f Dockerfile .
+docker push $ACCOUNT.dkr.ecr.us-gov-west-1.amazonaws.com/monitoring/blackbox:latest
