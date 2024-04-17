3662f966482832e97d049240a2ef3d76740ebf7b
RYLIE NICHOL <Rylie.Nichol@kc.frb.org>
2024-04-17 10:43:49 -0500

    Mimir initial config

--- prometheus/template_config.sh
+++ prometheus/template_config.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 proxy=${1}
+FEATURE_BRANCH=${2:-false}
 TEMPLATE=${3:-'prometheus_template.yml'}
 
 export HTTP_PROXY=${proxy}
+
+if $FEATURE_BRANCH; then
+  export ENV_DISPLAYNAME=`echo -n $BRANCH_NAME | sha1sum | cut -c1-8`
+else
+  export ENV_DISPLAYNAME=$BRANCH_NAME
+fi
 
 rm -f prometheus.yml temp.yml
 ( echo "cat <<EOF >prometheus.yml";
