d3640a140101902bf4d4529431538c295a946159
RYLIE NICHOL <Rylie.Nichol@kc.frb.org>
2024-05-06 10:51:04 -0500

    alloy templating script

--- alloy/template_config.sh
+++ alloy/template_config.sh
@@ -3,8 +3,8 @@
 proxy=${1}
 BRANCH_NAME=${2}
 FEATURE_BRANCH=${3:-false}
-TEMPLATE=${4:-'/config/blackbox_template.yml'}
-TEMPLATE=${5:-'/config/config.alloy'}
+TEMPLATE=${4:-'config/blackbox_template.yml'}
+TEMPLATE=${5:-'config/config.alloy'}
 
 export HTTP_PROXY=${proxy}
 
@@ -15,7 +15,7 @@ else
 fi
 
 rm -f blackbox.yml temp.yml
-( echo "cat <<EOF >/config/blackbox.yml";
+( echo "cat <<EOF >config/blackbox.yml";
   cat config/blackbox_template.yml;
   echo "EOF";
 ) >temp.yml
