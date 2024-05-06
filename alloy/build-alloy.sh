8c43b7526a0eb1e8dcc514b9fcd1d0a5f24bccf4
RYLIE NICHOL <Rylie.Nichol@kc.frb.org>
2024-05-06 10:56:47 -0500

    alloy templating script

--- alloy/build-alloy.sh
+++ alloy/build-alloy.sh
@@ -5,6 +5,7 @@ ACCOUNT=${1}
 FEATURE_BRANCH=${2:-false}
 TOKEN=`aws ssm get-parameter --name /jenkins/service/api-token --with-decryption --output text --query Parameter.Value --region us-gov-west-1`
 
+rm -rf /config/template-config.sh
 if $FEATURE_BRANCH; then
   export ENV_DISPLAYNAME=`echo -n $BRANCH_NAME | sha1sum | cut -c1-8`
 else
